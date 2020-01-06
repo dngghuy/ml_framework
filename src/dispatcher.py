5ad0191c83357622c6abd39a3c7da4a7951259a7
huygdng <huygdng@gmail.com>
2020-01-07 00:00:57 +0700

    Create some models

---
+++ src/dispatcher.py
@@ -0,0 +1,11 @@
+from sklearn import ensemble
+0.75091
+
+MODELS = {
+    'randomforest': ensemble.RandomForestClassifier(n_estimators=200,
+                                                    n_jobs=-1,
+                                                    verbose=2),
+    'extratress': ensemble.ExtraTreesClassifier(n_estimators=200,
+                                                n_jobs=-1,
+                                                verbose=2)
+}
