11c6506665039b0bc9ca95092fec95d9e28ba850
Gia-Huy Dang <huydg@mti-tech.vn>
2020-01-30 17:49:36 +0700

    adding numerical preprocessing

--- src/numerical.py
+++ src/numerical.py
@@ -2,8 +2,7 @@
 This module handles the some preprocessing for numerical features.
 This module will be called by feature_generator.
 """
-import numpy as np
-from sklearn.base import BaseEstimator, TransformerMixin
+from .basic_import import np, TransformerMixin
 
 
 def fill_na_mean(df, to_int=np.uint8, **kwargs):
@@ -84,41 +83,3 @@ class FillNA(TransformerMixin):
                 X[[col]] = self.style_callable(X[[col]], **self.style_callable_params)
 
             return X
-
-
-if __name__ == '__main__':
-    import pandas as pd
-    from . import dispatcher
-    from .feature_generator import Selector
-    from sklearn.pipeline import Pipeline, FeatureUnion
-    sample_train = pd.read_csv(dispatcher.SAMPLE_TRAIN_PATH)
-    print(sample_train.head(10))
-    print(sample_train.dtypes)
-    print(sample_train.isna().any())
-    sample_train_cols = list(sample_train.columns)
-    sample_train_select = ['bin_0', 'bin_1', 'bin_2']
-    sample_fill_pipeline = Pipeline([
-        ('selector', Selector(key=sample_train_select)),
-        ('fill_max', FillNA(style='max', to_int=np.uint8))
-    ])
-    sample_other_select = [i for i in sample_train_cols if i not in sample_train_select]
-    sample_selects = Pipeline([
-        ('selector', Selector(key=sample_other_select))
-    ])
-
-    feat_unions = FeatureUnion([
-        ('fill_nan', sample_fill_pipeline),
-        ('only_select', sample_selects)
-    ])
-    feature_process = Pipeline([
-        ('feats', feat_unions),
-    ])
-    print(sample_other_select)
-    new_sample = feature_process.fit_transform(sample_train)
-    print('#### After preprocessing')
-    new_sample_df = pd.DataFrame(new_sample)
-    new_sample_df.columns = sample_train_select + sample_other_select
-
-    print(new_sample_df.head(10))
-    print(new_sample_df.dtypes)
-    print(new_sample_df.isna().any())
