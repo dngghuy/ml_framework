7ae32f93c93b5fb5c3e3c1e0f882f3cb4bfd0996
Gia-Huy Dang <huydg@mti-tech.vn>
2020-01-30 17:48:48 +0700

    minor modification

--- src/dispatcher.py
+++ src/dispatcher.py
@@ -7,10 +7,16 @@ DATA_PATH = pathlib.Path(__file__).parent.parent / 'data'
 
 
 # PER-PRJ SETTINGS
+# CAT IN THE DAT II
 # TRAIN, TEST, OTHER DATA PATH SETTINGS
 SAMPLE_TRAIN_PATH = DATA_PATH / 'cat_in_the_dat' / 'train.csv'
 SAMPLE_TEST_PATH = DATA_PATH / 'cat_in_the_dat' / 'test.csv'
 SAMPLE_SUB_PATH = DATA_PATH / 'cat_in_the_dat' / 'sample_submission.csv'
+
+# BACKORDER
+# TRAIN, TEST
+BACKORDER_TRAIN_PATH = DATA_PATH / 'backorder' / 'training_set.csv'
+BACKORDER_TEST_PATH = DATA_PATH / 'backorder' / 'test_set_sample.csv'
 
 # MODEL SETTINGS
 
