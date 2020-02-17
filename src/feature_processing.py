d1f3f8074786d2c8d511b6c710329c70c2d7e9c5
huygdng <huygdng@gmail.com>
2020-02-17 23:17:57 +0700

    init baseline 2

--- src/feature_processing.py
+++ src/feature_processing.py
@@ -25,3 +25,26 @@ def get_seed(x):
     return int(x[1:3])
 
 
+def baseline_process_season_results(season_result):
+    season_win_result = season_result[['Season', 'WTeamID', 'WScore']]
+    season_lose_result = season_result[['Season', 'LTeamID', 'LScore']]
+    season_win_result.rename(columns={'WTeamID': 'TeamID', 'WScore': 'Score'}, inplace=True)
+    season_lose_result.rename(columns={'LTeamID': 'TeamID', 'LScore': 'Score'}, inplace=True)
+    season_result = pd.concat((season_win_result, season_lose_result)).reset_index(drop=True)
+
+    return season_result
+
+
+def baseline_merge_tourney_results_season_results(tourney_result, season_result):
+    tourney_result = pd.merge(tourney_result, season_score, left_on=['Season', 'WTeamID'],
+                              right_on=['Season', 'TeamID'], how='left')
+    tourney_result.rename(columns={'Score': 'WScoreT'}, inplace=True)
+    tourney_result = tourney_result.drop('TeamID', axis=1)
+    tourney_result = pd.merge(tourney_result, season_score, left_on=['Season', 'LTeamID'],
+                              right_on=['Season', 'TeamID'], how='left')
+    tourney_result.rename(columns={'Score': 'LScoreT'}, inplace=True)
+    tourney_result = tourney_result.drop('TeamID', axis=1)
+
+    return tourney_result
+
+
